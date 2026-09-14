b81378d4bf4bb0949e3b1672f21938c46cc7dafd
Afzal Akbari <afzalakbari2017@gmail.com>
2026-09-14 13:04:02 +0430

    Add full Pashto translations (po+mo) and Persian footer strings
    - Ship compiled Gettext MO catalogs for ps: client, mshop,
      controller/frontend, admin, language, currency, country
    - Extend client/fa with footer/nav strings (Legal, Terms, About us...)
    - Register controller/frontend i18n domain in manifest
    - Footer and nav now translate via aitrans(); ps joins RTL locales

--- resources/views/vendor/shop/base.blade.php
+++ resources/views/vendor/shop/base.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="no-js" lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{ in_array(app()->getLocale(), ['ar', 'az', 'dv', 'fa', 'he', 'ku', 'ur']) ? 'rtl' : 'ltr' }}">
+<html class="no-js" lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{ in_array(app()->getLocale(), ['ar', 'az', 'dv', 'fa', 'he', 'ku', 'ps', 'ur']) ? 'rtl' : 'ltr' }}">
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -15,7 +15,7 @@
 		<link rel="apple-touch-icon" href="{{ asset('vendor/shop/themes/default/assets/apple-touch-icon.png') }}">
 		<meta name="theme-color" content="#1c5b3a">
 
-		@if( in_array(app()->getLocale(), ['ar', 'az', 'dv', 'fa', 'he', 'ku', 'ur']) )
+		@if( in_array(app()->getLocale(), ['ar', 'az', 'dv', 'fa', 'he', 'ku', 'ps', 'ur']) )
 			<link type="text/css" rel="stylesheet" href="{{ asset('vendor/shop/themes/default/app.rtl.css?v=' . config( 'shop.version', 1 ) ) }}">
 		@else
 			<link type="text/css" rel="stylesheet" href="{{ asset('vendor/shop/themes/default/app.css?v=' . config( 'shop.version', 1 ) ) }}">
@@ -40,8 +40,8 @@
 	</head>
 	<body class="{{ $page ?? '' }}">
 		<nav class="navbar navbar-expand-md navbar-top">
-			<a class="navbar-brand" href="/" title="{{ __('To the home page') }}">
-				<img src="{{ asset( app( 'aimeos.context' )->get()->config()->get( 'resource/fs-media/baseurl' ) . '/' . ( app( 'aimeos.context' )->get()->locale()->getSiteItem()->getLogo() ?: '../vendor/shop/themes/default/assets/logo.png' ) ) }}" height="40" alt="{{ __('To the home page') }}">
+			<a class="navbar-brand" href="/" title="{{ aitrans('To the home page') }}">
+				<img src="{{ asset( app( 'aimeos.context' )->get()->config()->get( 'resource/fs-media/baseurl' ) . '/' . ( app( 'aimeos.context' )->get()->locale()->getSiteItem()->getLogo() ?: '../vendor/shop/themes/default/assets/logo.png' ) ) }}" height="40" alt="{{ aitrans('To the home page') }}">
 			</a>
 
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-top" aria-controls="navbar-top" aria-expanded="false" aria-label="Toggle navigation">
@@ -56,16 +56,16 @@
 
 			<ul class="navbar-nav">
 				@if (Auth::guest() && config('app.shop_registration'))
-					<li class="nav-item register"><a class="nav-link" href="{{ airoute( 'register' ) }}" title="{{ __( 'Register' ) }}"><span class="name">{{ __('Register') }}</span></a></li>
+					<li class="nav-item register"><a class="nav-link" href="{{ airoute( 'register' ) }}" title="{{ aitrans( 'Register' ) }}"><span class="name">{{ aitrans('Register') }}</span></a></li>
 				@endif
 				@if (Auth::guest())
-					<li class="nav-item login"><a class="nav-link" href="{{ airoute( 'login' ) }}" title="{{ __( 'Login' ) }}"><span class="name">{{ __( 'Login' ) }}</span></a></li>
+					<li class="nav-item login"><a class="nav-link" href="{{ airoute( 'login' ) }}" title="{{ aitrans( 'Login' ) }}"><span class="name">{{ aitrans( 'Login' ) }}</span></a></li>
 				@else
 					<li class="nav-item login profile dropdown">
-						<a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false" title="{{ __( 'Account' ) }}"><span class="name">{{ __( 'Account' ) }}</span> <span class="caret"></span></a>
+						<a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false" title="{{ aitrans( 'Account' ) }}"><span class="name">{{ aitrans( 'Account' ) }}</span> <span class="caret"></span></a>
 						<ul class="dropdown-menu dropdown-menu-end" role="menu">
-							<li class="dropdown-item"><a class="nav-link" href="{{ airoute( 'aimeos_shop_account' ) }}"><span class="name">{{ __( 'Profile' ) }}</span></a></li>
-							<li class="dropdown-item"><form id="logout" action="{{ airoute( 'logout' ) }}" method="POST">{{ csrf_field() }}<button class="nav-link"><span class="name">{{ __( 'Logout' ) }}</span></button></form></li>
+							<li class="dropdown-item"><a class="nav-link" href="{{ airoute( 'aimeos_shop_account' ) }}"><span class="name">{{ aitrans( 'Profile' ) }}</span></a></li>
+							<li class="dropdown-item"><form id="logout" action="{{ airoute( 'logout' ) }}" method="POST">{{ csrf_field() }}<button class="nav-link"><span class="name">{{ aitrans( 'Logout' ) }}</span></button></form></li>
 						</ul>
 					</li>
 				@endif
@@ -90,30 +90,30 @@
 						<div class="row">
 							<div class="col-sm-6 footer-left">
 								<div class="footer-block">
-									<h2 class="pb-3" aria-label="{{ __('Legal information') }}">{{ __( 'LEGAL' ) }}</h2>
-									<p><a href="{{ airoute(config('shop.client.html.cms.page.url.target', 'aimeos_page'), ['path' => 'terms']) }}">{{ __( 'Terms & Conditions' ) }}</a></p>
-									<p><a href="{{ airoute(config('shop.client.html.cms.page.url.target', 'aimeos_page'), ['path' => 'privacy']) }}">{{ __( 'Privacy Notice' ) }}</a></p>
-									<p><a href="{{ airoute(config('shop.client.html.cms.page.url.target', 'aimeos_page'), ['path' => 'cancel']) }}">{{ __( 'Cancellation' ) }}</a></p>
+									<h2 class="pb-3" aria-label="{{ aitrans('Legal information') }}">{{ aitrans( 'LEGAL' ) }}</h2>
+									<p><a href="{{ airoute(config('shop.client.html.cms.page.url.target', 'aimeos_page'), ['path' => 'terms']) }}">{{ aitrans( 'Terms & Conditions' ) }}</a></p>
+									<p><a href="{{ airoute(config('shop.client.html.cms.page.url.target', 'aimeos_page'), ['path' => 'privacy']) }}">{{ aitrans( 'Privacy Notice' ) }}</a></p>
+									<p><a href="{{ airoute(config('shop.client.html.cms.page.url.target', 'aimeos_page'), ['path' => 'cancel']) }}">{{ aitrans( 'Cancellation' ) }}</a></p>
 								</div>
 							</div>
 							<div class="col-sm-6 footer-center">
 								<div class="footer-block">
-									<h2 class="pb-3" aria-label="{{ __('About the company') }}">{{ __( 'ABOUT US' ) }}</h2>
-									<p><a href="{{ airoute(config('shop.client.html.cms.page.url.target', 'aimeos_page'), ['path' => 'contact']) }}">{{ __( 'Contact us' ) }}</a></p>
+									<h2 class="pb-3" aria-label="{{ aitrans('About the company') }}">{{ aitrans( 'ABOUT US' ) }}</h2>
+									<p><a href="{{ airoute(config('shop.client.html.cms.page.url.target', 'aimeos_page'), ['path' => 'contact']) }}">{{ aitrans( 'Contact us' ) }}</a></p>
 									@if( $shopEmail = app( 'aimeos.context' )->get()->locale()->getSiteItem()->getConfigValue( 'client/html/email/from-email' ) )
 										<p><a href="mailto:{{ $shopEmail }}">{{ $shopEmail }}</a></p>
 									@endif
-									<p><a href="{{ airoute(config('shop.client.html.cms.page.url.target', 'aimeos_page'), ['path' => 'about']) }}">{{ __( 'Company' ) }}</a></p>
+									<p><a href="{{ airoute(config('shop.client.html.cms.page.url.target', 'aimeos_page'), ['path' => 'about']) }}">{{ aitrans( 'Company' ) }}</a></p>
 								</div>
 							</div>
 						</div>
 					</div>
 					<div class="col-md-4 footer-right">
 						<div class="footer-block">
-							<a class="logo" href="/" title="{{ __('To the home page') }}">
-								<img src="{{ asset( app( 'aimeos.context' )->get()->config()->get( 'resource/fs-media/baseurl' ) . '/' . ( app( 'aimeos.context' )->get()->locale()->getSiteItem()->getLogo() ?: '../vendor/shop/themes/default/assets/logo.png' ) ) }}" height="40" alt="{{ __('To the home page') }}">
+							<a class="logo" href="/" title="{{ aitrans('To the home page') }}">
+								<img src="{{ asset( app( 'aimeos.context' )->get()->config()->get( 'resource/fs-media/baseurl' ) . '/' . ( app( 'aimeos.context' )->get()->locale()->getSiteItem()->getLogo() ?: '../vendor/shop/themes/default/assets/logo.png' ) ) }}" height="40" alt="{{ aitrans('To the home page') }}">
 							</a>
-							<div class="social" aria-label="{{ __('Social media links') }}">
+							<div class="social" aria-label="{{ aitrans('Social media links') }}">
 								<p><a href="#" class="sm facebook" title="Facebook" rel="noopener">Facebook</a></p>
 								<p><a href="#" class="sm twitter" title="Twitter" rel="noopener">Twitter</a></p>
 								<p><a href="#" class="sm instagram" title="Instagram" rel="noopener">Instagram</a></p>
@@ -127,7 +127,7 @@
 
 
 
-		<a id="toTop" class="back-to-top" href="#" title="{{ __( 'Back to top' ) }}">
+		<a id="toTop" class="back-to-top" href="#" title="{{ aitrans( 'Back to top' ) }}">
 			<div class="top-icon"></div>
 		</a>
 
